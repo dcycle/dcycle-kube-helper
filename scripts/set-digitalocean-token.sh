5ac8db0f1fae3695bb1ebe342ba6bd47a2807127
alberto56 <albert@mediatribe.net>
2020-04-21 14:57:08 -0400

    set-digitalocean-token.sh

---
+++ scripts/set-digitalocean-token.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Remember your digitalocean token in your home folder.
+#
+set -e
+
+if [ -z "$DOTOKEN" ] || [ -z "$DOCLUSTERUUID" ] || [ -z "$DOTOKENNAME" ]; then
+  >&2 echo "Before running this script please run:"
+  >&2 echo "export DOTOKEN=<your digitan ocean token here>"
+  >&2 echo "export DOCLUSTERUUID=<your digital ocean cluster uuid here>"
+  >&2 echo "export DOTOKENNAME=<name your token here>"
+  exit 1;
+fi
+
+mkdir -p "$HOME/.digitalocean"
+echo "DOTOKEN=$DOTOKEN" > "$HOME/.digitalocean/$DOTOKENNAME"
+echo "DOCLUSTERUUID=$DOCLUSTERUUID" >> "$HOME/.digitalocean/$DOTOKENNAME"
+# Remove read access except for yourself.
+chmod go-r "$HOME"/.digitalocean/tutorial-token
+
+echo ""
+echo "All done, to see your token file check:"
+echo ""
+echo '    cat "$HOME/.digitalocean/tutorial-token"'
+echo ""
+echo "Note that your token is stored in the clear; use an encrypted secrets"
+echo "manager for production or sensitive clusters."
+echo ""
