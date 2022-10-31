69b41f69623d235b6758fb61477071d5919aa0c0
Albert Albala <albert@mediatribe.net>
2022-10-31 13:12:47 -0400

    Update cert-manager for 2022-10

--- scripts/install-cert-manager.sh
+++ scripts/install-cert-manager.sh
@@ -6,12 +6,12 @@ set -e
 
 echo ""
 echo "About to try to install the cert manager"
-echo "Don't worry about the 'Failed to download OpenAPI' error!"
+echo "as per https://cert-manager.io/docs/installation/helm/"
 echo ""
 
-kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.13/deploy/manifests/00-crds.yaml
-./scripts/helm.sh repo add jetstack https://charts.jetstack.io
-./scripts/helm.sh upgrade --install cert-manager jetstack/cert-manager --version v0.13.1
+ ./scripts/helm.sh repo add jetstack https://charts.jetstack.io
+ ./scripts/helm.sh repo update
+ ./scripts/helm.sh upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.10.0 --set installCRDs=true
 
 echo ""
 echo "All done!"
